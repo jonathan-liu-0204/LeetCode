468765db696d31d6c7886c3b71e5b2598b7b8167
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-04-25 19:00:13 +0800

    Problem #633

---
+++ Two Pointers/633. Sum of Square Numbers (Easy)/633-Sum-of-Square-Numbers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        
+        // bool result{false};        
+
+        int left = 0;
+        int right = (int)sqrt(c);
+        
+        if(c < 0){
+            return false;
+        }
+
+        while(left <= right){
+
+            // int cal = left^2 + right^2;
+            long long cal = (long long) left * left + right * right;
+            
+            if(cal == c){
+                // result = true;
+                return true;
+            }
+            else if(cal < c){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return false;
+    }
+};
