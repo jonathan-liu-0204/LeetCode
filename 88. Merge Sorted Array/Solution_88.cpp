cfcb23fb00254eb7df7d3c2d2ef9ba49ac5c1adb
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-02 21:01:15 +0800

    problem 88

---
+++ 88. Merge Sorted Array/Solution_88.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int i = 0; //pointer for nums1
+        int j = 0; //pointer for nums2
+        
+        while(j < n){
+            
+            i = 0;
+            cout << "j: " << j << "   ";
+            
+            while(i < m+n){
+                if(i == m+j){
+                    nums1.insert(nums1.begin() + i, nums2[j]);
+                    cout << "position to insert: " << i << " " << "insert value:  " << nums2[j] << endl;
+                    nums1.pop_back();
+                    break;
+                }
+                else if(nums2[j] <= nums1[i]){
+                    nums1.insert(nums1.begin() + i, nums2[j]);
+                    cout << "position to insert: " << i << " " << "insert value:  " << nums2[j] << endl;
+                    nums1.pop_back();
+                    break;
+                }
+                i++;
+            }
+            
+            j++;
+        }
+    }
+};
