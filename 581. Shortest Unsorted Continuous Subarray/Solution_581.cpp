b86e538ffb7f0e65597966d9d03887a7eb33d7cb
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-03 23:35:26 +0800

    Problem 581

--- 581. Shortest Unsorted Continuous Subarray/Solution_581.cpp
+++ 581. Shortest Unsorted Continuous Subarray/Solution_581.cpp
@@ -2,5 +2,24 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         
+        vector<int> sorted_nums = nums;
+        sort(sorted_nums.begin(), sorted_nums.end());
+        
+        int left = sorted_nums.size();
+        int right = 0;
+        
+        for(int i = 0; i < sorted_nums.size(); i++){
+            if(sorted_nums[i] != nums[i]){
+                left = min(left, i);
+                right = max(right, i);
+            }
+        }
+                
+        if(right < left){
+            return 0;
+        }
+        else{
+            return (right - left + 1);
+        }
     }
 };
