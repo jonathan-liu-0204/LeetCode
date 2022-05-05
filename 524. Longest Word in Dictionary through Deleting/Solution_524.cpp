84ee6d706ce66b9b7d01ef45a18c0d41a724446a
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-05 21:26:41 +0800

    Problem 524

--- 524. Longest Word in Dictionary through Deleting/Solution_524.cpp
+++ 524. Longest Word in Dictionary through Deleting/Solution_524.cpp
@@ -2,5 +2,41 @@ class Solution {
 public:
     string findLongestWord(string s, vector<string>& dictionary) {
         
+        string longest = "";
+        
+        for(int i = 0; i < dictionary.size(); i++){
+            // cout << dictionary[i] << "  " << dictionary[i].size() << endl;
+            
+            int count = 0;
+                        
+            cout << "currently at: " << dictionary[i] << endl;
+            
+            int index1 = 0;
+            int index2 = 0;
+            
+            while(index1 < dictionary[i].size() && index2 < s.size()){
+                if(s[index2] == dictionary[i][index1]){
+                    count++;
+                    index1++;
+                    index2++;
+                }
+               else{
+                    index2++;      
+               }
+            }
+            
+            if(count == dictionary[i].size()){
+                if(count > longest.size()){
+                    longest = dictionary[i];
+                }
+                else if(count == longest.size()){
+                    if(dictionary[i] < longest){
+                        longest = dictionary[i];
+                    }
+                }
+            }
+        }
+        
+        return longest;
     }
 };
