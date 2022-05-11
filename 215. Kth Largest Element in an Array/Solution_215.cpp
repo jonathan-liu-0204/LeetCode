ce15bf3504cdda2a85f06535427a4055320bb0f1
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-11 09:54:27 +0800

    my solution of 215

--- 215. Kth Largest Element in an Array/Solution_215.cpp
+++ 215. Kth Largest Element in an Array/Solution_215.cpp
@@ -2,5 +2,21 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         
+        sort(nums.begin(), nums.end());
+        
+        int big = nums[nums.size() - 1] + 1;
+        int count = 0;
+        
+        for(int i = nums.size() - 1; i >= 0; i--){
+            if(nums[i] <= big){
+                count++;
+                big = nums[i];
+            }
+            
+            if(count == k){
+                return big;
+            }
+        }
+        return 0;
     }
 };
