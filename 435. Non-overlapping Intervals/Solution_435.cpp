1300006ef54db370cf271c8d07e0e1a1f68fe9d6
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-19 16:00:53 +0800

    solution 435

--- 435. Non-overlapping Intervals/Solution_435.cpp
+++ 435. Non-overlapping Intervals/Solution_435.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
+    
+    static bool sortcol(const vector<int>& v1, const vector<int>& v2){
+            return v1[1] < v2[1];
+    }
+    
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         
+        sort(intervals.begin(), intervals.end(), sortcol);
+        
+        int count = 0;
+        int previous = -60000;
+        
+        for(int i = 0; i < intervals.size(); i++){
+            if(intervals[i][0] >= previous){
+                previous = intervals[i][1];
+            }
+            else{
+                count++;
+            }
+            // cout << "i: " << i << "  " << intervals[i][0] << " " << intervals[i][1] << " previous: " << previous << " count: " << count << endl;
+        }
+        
+        return count;
     }
 };
