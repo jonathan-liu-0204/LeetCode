ed85fb04da1cd4b861b4b9ca6f93e135f62554c8
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-05-22 10:52:32 +0800

    solution 121

--- 121. Best Time to Buy and Sell Stock/Solution-121.cpp
+++ 121. Best Time to Buy and Sell Stock/Solution-121.cpp
@@ -2,5 +2,21 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
+        int minbuy = prices[0];
+        int profit = 0;
+        
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] < minbuy){
+                minbuy = prices[i];
+            }
+            else{
+                profit = max(profit, (prices[i] - minbuy));
+            }
+        }
+        
+        return profit;
     }
 };
+
+// Runtime: 237 ms, faster than 11.79% of C++ online submissions for Best Time to Buy and Sell Stock.
+// Memory Usage: 93.3 MB, less than 90.01% of C++ online submissions for Best Time to Buy and Sell Stock.
