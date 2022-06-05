4e8cbb9a0fbf23c5947a9f52b815f4c375ce562a
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-06-05 15:17:27 +0800

    solution to problem 69

--- 69. Sqrt(x)/Solution-69.cpp
+++ 69. Sqrt(x)/Solution-69.cpp
@@ -2,5 +2,25 @@ class Solution {
 public:
     int mySqrt(int x) {
         
+        unsigned long i = 0;
+                
+        while(i <= (x/2) + 1){
+            
+            
+            if(i*i < x && (i+1)*(i+1) > x){
+                return i;
+            }
+            else if(i*i < x){
+                i++;
+            }
+            
+            if(i*i == x){
+                return i;
+            }
+        }
+        return 0;
     }
 };
+
+// Runtime: 68 ms, faster than 5.31% of C++ online submissions for Sqrt(x).
+// Memory Usage: 5.9 MB, less than 25.99% of C++ online submissions for Sqrt(x).
